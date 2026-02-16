04bbec632105b285946f8c46bc84ba404cba8c39
Samiran Sarkar <samiransarka123@gmail.com>
2026-02-16 19:41:46 +0530

    rtywersd

--- src/hooks/useUserRole.ts
+++ src/hooks/useUserRole.ts
@@ -19,6 +19,7 @@ export const useUserRole = () => {
     return {
         isLoading,
         isInterviewer: userData?.role === "interviewer",
-        
+        isCandidate: userData?.role === "candidate",
+
     }
 }
